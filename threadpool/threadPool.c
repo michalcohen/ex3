5371eac4ec99f3567967ee076350db34f68c7406
michalcohen <cohenmelany1@gmail.com>
2015-05-14 11:17:36 +0300

    initialization of threadPool.c

---
+++ threadpool/threadPool.c
@@ -0,0 +1,14 @@
+#include "threadPool.h"
+
+ThreadPool* tpCreate(int numOfThreads){
+	return NULL;
+}
+
+void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks){
+	
+}
+
+int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param){
+	
+	return 0;
+}
